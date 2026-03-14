82e8153d5d5b9040a627b767f210de84ee193141
Vins <j4k4l4123456789@gmail.com>
2026-03-14 15:14:51 +0700

    Penjelasan perubahan Anda

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [AuthController::class, 'dashboard']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
